7ee1f9240d038530334588dc3e95f6942ce54ca9
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-03-04 13:08:46 -0500

    Created a function that returns the license section of the README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,13 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return ``;
+  } else {
+    return `The licence used for this project was ${license}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {}
